45b93e788ac6136022aaa32705e6d3f7d1549198
Andrey Burdak <burdak1994@gmail.com>
2026-06-07 23:56:27 +0300

    add: logging

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,23 +1,20 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
 import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
 
-import java.util.Collections;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private HashMap<String, User> users = new HashMap<>();
-
-    @GetMapping
-    public HashMap<String, User> getUsers() {
-        return users;
-    }
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final HashMap<String, User> users = new HashMap<>();
 
     @PostMapping
     public User createUser(@RequestBody User user) {
@@ -26,17 +23,26 @@ public class UserController {
         } else if (user.getEmail() == null || user.getEmail().isBlank()) {
             throw new InvalidEmailException("Адрес почты равен null или пустой строке");
         } else
-            users.put(user.getEmail(), user);
+            log.debug("Добавлен пользователь: {}", user);
+        users.put(user.getEmail(), user);
         return user;
+    }
+
+    @GetMapping
+    public Collection<User> getUsers() {
+        log.debug("Количество пользователей: {}", users.size());
+        return users.values();
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
         if (user.getEmail() == null || user.getEmail().isBlank()) {
             throw new InvalidEmailException("Адрес почты равен null или пустой строке");
-        } else
+        } else {
+            log.debug("Обновлен пользователь: {}", user);
             users.put(user.getEmail(), user);
-        return user;
+            return user;
+        }
     }
 
 }
