036cbd817ca2b09ee7c0dd13c2eb089411f11a1d
Andrey Burdak <burdak1994@gmail.com>
2026-06-09 23:37:13 +0300

    add: добавить решения из ветки add_handling

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.catsgram.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import java.util.List;
 
 import static ru.yandex.practicum.catsgram.Constants.SORTS;
 
+@Slf4j
 @RestController()
 @RequestMapping("/feed/friends")
 public class PostFeedController {
@@ -26,7 +30,7 @@ public class PostFeedController {
 
     @PostMapping
     List<Post> getFriendsFeed(@RequestBody FeedParams feedParams) {
-        if (!SORTS.contains(feedParams.getSort()) || feedParams.getFriendsEmails().isEmpty()) {
+        if (!SORTS.contains(feedParams.getSort()) || feedParams.getFriends().isEmpty()) {
             throw new IllegalArgumentException();
         }
         if (feedParams.getSize() == null || feedParams.getSize() <= 0) {
@@ -34,41 +38,48 @@ public class PostFeedController {
         }
 
         List<Post> result = new ArrayList<>();
-        for (String friendEmail : feedParams.getFriendsEmails()) {
+        for (String friendEmail : feedParams.getFriends()) {
             result.addAll(postService.findAllByUserEmail(friendEmail, feedParams.getSize(), feedParams.getSort()));
         }
         return result;
     }
-    @PostMapping("/feed/friends")
-    public List<Post> getFriends(@RequestBody String input) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        FriendsParam friendsParam;
-        try {
-            String paramsFromInput = objectMapper.readValue(input, String.class);
-            friendsParam = objectMapper.readValue(paramsFromInput, FriendsParam.class);
+//    @PostMapping("/feed/friends")
+//    public List<Post> getFriends(@RequestBody String input) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        FeedParams friendsParam;
+//        try {
+//            String paramsFromInput = objectMapper.readValue(input, String.class);
+//            friendsParam = objectMapper.readValue(paramsFromInput, FeedParams.class);
+//
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException("Невалидные значения в строке body Json", e);
+//        }
+//
+//        if (friendsParam != null) {
+//            List<Post> result = new ArrayList<>();
+//
+//            for (String email : friendsParam.getFriends()) {
+//                result.addAll(postService.findAllByUserEmail(email, friendsParam.getSize(), friendsParam.getSort()));
+//            }
+//
+//            log.debug("Получен запрос на получение ленты друзей с параметрами: sort={}, size={}, friends={}", friendsParam.sort, friendsParam.size, friendsParam.getFriends());
+//            return result;
+//        } else {
+//            throw new RuntimeException("ошибка при создании DTO FeedParams");
+//        }
+//
+//    }
 
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Невалидные значения в строке body Json", e);
-        }
+    @Data
+    private static class FeedParams {
+        private final String sort;
+        private final Integer size;
+        private final List<String> friends;
 
-        if (friendsParam != null) {
-            List<Post> result = new ArrayList<>();
 
-            for (String email : friendsParam.friends()) {
-                result.addAll(postService.findPostsByEmails(email, friendsParam.sort, friendsParam.size));
-            }
 
-            log.debug("Получен запрос на получение ленты друзей с параметрами: sort={}, size={}, friends={}", friendsParam.sort, friendsParam.size, friendsParam.friends());
-            return result;
-        } else {
-            throw new RuntimeException("ошибка при создании DTO FriendsParam");
-        }
 
     }
-
-    private record FriendsParam(String sort, Integer size, List<String> friends) {
-    }
-}
 
 
 
